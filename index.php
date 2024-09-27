343fd4aa14e1bcecfe4a9f88ce184c2099d917a9
huudan2004-prod <dannhph35080@fpt.edu.vn>
2024-09-27 22:17:21 +0700

    Dan 27-9

--- index.php
+++ index.php
@@ -6,6 +6,93 @@ include "model/taikhoan.php";
 if (isset($_GET['act']) && ($_GET['act'] != "")) {
     $act = $_GET['act'];
     switch ($act) {
+        case "order":
+            if (isset($_SESSION['cart'])) {
+                $cart = $_SESSION['cart'];
+                // print_r($cart);
+                if (isset($_POST['order_confirm'])) {
+                    $txthoten = $_POST['txthoten'];
+                    $txttel = $_POST['txttel'];
+                    $txtemail = $_POST['txtemail'];
+                    $txtaddress = $_POST['txtaddress'];
+                    $pttt = $_POST['pttt'];
+                    if (isset($_SESSION['user'])) {
+                        $id_user = $_SESSION['user']['id'];
+                    } else {
+                        $id_user = 0;
+                    }
+                    $idBill = addOrder($id_user, $txthoten, $txttel, $txtemail, $txtaddress, $_SESSION['resultTotal'], $pttt);
+                    foreach ($cart as $item) {
+                        addOrderDetail($idBill, $item['id'], $item['price'], $item['quantity'], $item['price'] * $item['quantity']);
+                    }
+                    unset($_SESSION['cart']);
+                    $_SESSION['success'] = $idBill;
+                    include "client/success.php";
+                } else {
+                    include "client/order.php";
+                }
+            } else {
+            }
+            break;
+        case "order2":
+            if (isset($_SESSION['buy'])) {
+                $buyNowItems = $_SESSION['buy'];
+
+                if (isset($_POST['order_confirm2'])) {
+
+                    $txthoten = $_POST['txthoten'];
+                    $txttel = $_POST['txttel'];
+                    $txtemail = $_POST['txtemail'];
+                    $txtaddress = $_POST['txtaddress'];
+                    $pttt = $_POST['pttt'];
+
+                    $id_user = isset($_SESSION['user']) ? $_SESSION['user']['id'] : 0;
+
+                    $idBill = addOrder($id_user, $txthoten, $txttel, $txtemail, $txtaddress, $_SESSION['resultTotal'], $pttt);
+                    foreach ($buyNowItems as $item) {
+                        addOrderDetail($idBill, $item['id'], $item['price'], $item['quantity'], $item['price'] * $item['quantity']);
+                    }
+                    unset($_SESSION['buy']);
+                    $_SESSION['success2'] = $idBill;
+                    include "client/success2.php";
+                } else {
+                    // Display the "Buy Now" page
+                    include "client/order2.php";
+                }
+            } else {
+            }
+            break;
+        case "success2":
+            if (isset($_SESSION['success2'])) {
+                include 'client/success2.php';
+            } else {
+                header("Location: index.php");
+            }
+            break;
+        case "success":
+            if (isset($_SESSION['success'])) {
+                include 'client/success.php';
+            } else {
+                header("Location: index.php");
+            }
+            break;
+        case "listCart":
+            // Kiểm tra xem giỏ hàng có dữ liệu hay không
+            if (!empty($_SESSION['cart'])) {
+                $cart = $_SESSION['cart'];
+                // Tạo mảng chứa ID các sản phẩm trong giỏ hàng
+                $productId = array_column($cart, 'id');
+                // Chuyển đôi mảng id thành một cuỗi để thực hiện truy vấn
+                $idList = implode(',', $productId);
+                // Lấy sản phẩm trong bảng sản phẩm theo id
+                $dataDb = loadone_sanphamCart($idList);
+                // var_dump($dataDb);
+            }
+            include "client/listCartOrder.php";
+            break;
+        case "buyNow":
+            include "client/buyNow.php";
+            break;
         case 'dangky':
             if ((isset($_POST['dangky'])) && ($_POST['dangky'])) {
                 $email = $_POST['email'];
