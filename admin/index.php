2dd76980d2f4d9ef929f4bccc5f4bcc8d942b851
cuongbv-11 <vc13050904@gmail.com>
2024-10-21 14:22:11 +0700

    fix delete don hang

--- admin/index.php
+++ admin/index.php
@@ -29,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 } else {
                     echo 'Access denied!';
                 }
-                exit(); // Exit to prevent further output
+                exit();
         }
     }
 }
